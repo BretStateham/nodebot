3447d5a897cbb6b8ce5a924991cf7363ee24e264
BretStateham <Bret.Stateham@microsoft.com>
2016-08-19 17:01:43 -0500

    initial commit

---
+++ app.js
@@ -0,0 +1,34 @@
+var restify = require('restify');
+var builder = require('botbuilder');
+
+//====================
+//Bot Setup
+//====================
+
+//Setup restify server
+var server = restify.createServer();
+server.listen(
+    process.env.port ||
+    process.env.PORT ||
+    3978, 
+    function() {
+        console.log('%s listening to %s', server.name, server.url);
+    }
+);
+
+//Create chat bot 
+var connector = new builder.ChatConnector({
+    appId: process.env.MICROSOFT_APP_ID,
+    appPassword: process.env.MICROSOFT_APP_PASSWORD
+});
+var bot = new builder.UniversalBot(connector);
+server.post('/api/messages',connector.listen());
+
+//====================
+//Bots Dialogs
+//====================
+
+bot.dialog('/',function(session){
+    //console.log(session.message);
+    session.send("Hello, " + session.message.user.name + '!  ' + session.message.text + ' to you too!');
+})
